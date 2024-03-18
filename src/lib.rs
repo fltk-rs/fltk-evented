eeb8fa3844861c48d26401bbbb020f2fa27cb09e
Mohammed Alyousef <may642_2000@hotmail.com>
2024-03-19 00:29:07 +0300

    fix example causing build failure

--- src/lib.rs
+++ src/lib.rs
@@ -5,11 +5,48 @@
 ## Async usage
 This crate provides an AsyncListener which can be used in async contexts. This requires enabling either the tokio or async-std features. You can check the examples directory for an example on usage.
 
-```rust
-"##,
-include_str!("../examples/async_examples/tokio_ex/src/main.rs"),
-r##"
+```rust,ignore
+use fltk::{prelude::*, *};
+use fltk_evented::AsyncListener;
+
+#[tokio::main]
+async fn main() -> Result<(), reqwest::Error> {
+    let mut buf = text::TextBuffer::default();
+    let a = app::App::default().with_scheme(app::Scheme::Gtk);
+    app::set_font_size(20);
+
+    let mut wind = window::Window::default()
+        .with_size(400, 300)
+        .center_screen()
+        .with_label("Counter");
+    let col = group::Pack::default()
+        .with_size(400, 300)
+        .center_of_parent()
+        .with_type(group::PackType::Vertical);
+    let mut editor = text::TextEditor::default().with_size(0, 240);
+    editor.set_buffer(buf.clone());
+    let getter: AsyncListener<_> = button::Button::default()
+        .with_label("Get")
+        .with_size(0, 60)
+        .into();
+    col.end();
+    wind.end();
+    wind.show();
+
+    while a.wait() {
+        if getter.triggered().await {
+            let text = reqwest::get("https://www.rust-lang.org")
+                .await?
+                .text()
+                .await?;
+            buf.set_text(&text);
+        }
+    }
+    Ok(())
+}
 ```
+
+The repo contains an async-std example as well.
 "##))]
 #![allow(clippy::needless_doctest_main)]
 
